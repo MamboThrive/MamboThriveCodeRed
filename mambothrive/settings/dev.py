7b37520a0649c1cbd60329f3d919c3b97ee7dae8
Manho Joung <MamboFamily2024@gmail.com>
2025-06-26 23:48:04 -0700

    initial commit

---
+++ mambothrive/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zgtg)gph&8d8nf)kwq$1ls!h^ds&a_64a)tv4n5z#*7_0p26tz"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
